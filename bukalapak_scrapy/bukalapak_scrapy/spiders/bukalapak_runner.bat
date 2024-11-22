7ebb727d84d3df56ab44450a94dc5901d5d89a83
rampagezero <andika.kristianto-2018@fst.unair.ac.id>
2024-11-22 11:42:55 +0700

    add new bat

--- bukalapak_scrapy/bukalapak_scrapy/spiders/bukalapak_runner.bat
+++ bukalapak_scrapy/bukalapak_scrapy/spiders/bukalapak_runner.bat
@@ -1,5 +1,5 @@
-call "D:\Python Scripts\Project Scrapy\scrape_proj\Scripts\activate.bat"
 echo activate project environment .....
+call "D:\Python Scripts\Project Scrapy\scrape_proj\Scripts\activate.bat"
 echo virtual environment activated!
 cd "D:\Python Scripts\Project Scrapy\scrapy_ecom_new\scrapy_ecom\bukalapak_scrapy\bukalapak_scrapy\spiders"
 scrapy crawl bukalapak
